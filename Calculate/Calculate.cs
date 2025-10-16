04a5364a41428639c40b2758171519087c1ebd08
88artem <147190913+88artem@users.noreply.github.com>
2025-10-16 14:38:03 +0500

    Add files via upload

---
+++ Calculate/Calculate.cs
@@ -0,0 +1,32 @@
+﻿using Calculates.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculates
+{
+    internal class Calculate : ICalculate
+    {
+        public double Sum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Div(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Mul(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Sub(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Calculate/Program.cs
@@ -0,0 +1,27 @@
+﻿using Calculates;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculate _calculate = new();
+        Console.WriteLine("Введите числа, а потом символ");
+        double num1 = double.Parse(Console.ReadLine());
+        double num2 = double.Parse(Console.ReadLine());
+        switch (char.Parse(Console.ReadLine()))
+        {
+            case '+':
+                Console.WriteLine(_calculate.Sum(num1, num2));
+                break;
+            case '*':
+                Console.WriteLine(_calculate.Mul(num1, num2));
+                break;
+            case '/':
+                Console.WriteLine(_calculate.Div(num1, num2));
+                break;
+            case '-':
+                Console.WriteLine(_calculate.Sub(num1, num2));
+                break;
+
+        }
+    }
+}
