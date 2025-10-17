4d1a6d1a5b3814b3f668996fe4c83023853fec68
88artem <147190913+88artem@users.noreply.github.com>
2025-10-17 17:06:26 +0500

    Add files via upload

---
+++ Calculate/Interface/ICalculateHard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculates.Interface
+{
+    internal interface ICalculateHard
+    {
+        public double Pow(double num1, double num2);
+        public double Sqrt(double num1);
+        public double Sin(double num1);
+        public double Cos(double num1);
+        public double Tan(double num1);
+    }
+}
